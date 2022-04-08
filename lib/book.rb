472e27f35409a98267e8144c5db435a333e8e73a
dean <diazdean21@gmail.com>
2022-04-07 19:07:12 -0500

    commit tree 5cf0b43f0891717bf1170f1335c108458827d084

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  attr_accessor :book, :title, :author, :page_count, :genre
+  def initialize (book)
+    @book = book
+    @title = "And Then There Were None"
+    @author_name = author
+    @page_count = page_count
+    @genre = genre
+  end
+  def turn_page
+    puts  "Flipping the page...wow, you read fast!"
+  end
+end
 
 
